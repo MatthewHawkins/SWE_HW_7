a08ec3fb5a5c143d9800e3dc5b6ae00622fc0fea
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-05-31 16:34:40 -0700

    Added a test to make sure Buzz works.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,7 @@
 def fizzBuzz(num):
     if(num % 3 == 0):
         return "Fizz"
+    elif(num % 5 == 0):
+        return "Buzz"
     else:
         return num
--- test-FizzBuzz.py
+++ test-FizzBuzz.py
@@ -7,6 +7,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fb.fizzBuzz(1), 1)
     def test_fizz(self):
         self.assertEqual(fb.fizzBuzz(3), "Fizz")
+    def test_buzz(self):
+        self.assertEqual(fb.fizzBuzz(5), "Buzz")
 
 
 if __name__ == "__main__":
